24caaf47301bb9853810c486cee0eb80980b2209
0x399 <pryimak.sviat@gmail.com>
2022-10-18 14:22:48 +0300

    123

--- src/Flowers/Flowers.java
+++ src/Flowers/Flowers.java
@@ -1,16 +1,24 @@
 package Flowers;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Flowers {
+    SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
     public String name;
     public Integer stem_len;
     private Double price;
     public Color color;
+    public Date ArrivalDate;
+    public static Integer Quantity;
 
-    public Flowers(String name, int stem_len, double price, Color color) {
+    public Flowers(String name, int stem_len, double price, Color color, String date) throws ParseException {
         this.name = name;
         this.stem_len = stem_len;
         this.price = price;
         this.color = color;
+        this.ArrivalDate = sdf.parse(String.valueOf(date));
     }
 
     public Double getPrice() {
@@ -23,10 +31,9 @@ public class Flowers {
 
     @Override
     public String toString() {
-        return "Flower: " +
-                "Name:'" + name + '\'' +
-                ", Stem Length: " + stem_len +
+        return name + ":" +
+                " Stem Length: " + stem_len +
                 ", Price: " + price +
-                ", Color: " + color;
+                ", Color: " + color + ", Delivered on: " + sdf.format(ArrivalDate);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,41 @@
 import Flowers.*;
 import Decorations.*;
+import Bouquet.*;
+
+import java.text.ParseException;
+import java.util.Scanner;
 
 public class Main {
-    static Flowers tulip = new Flowers("Tulip", 12, 40, Color.red);
+    static Flowers tulip1, tulip2;
+
+    static Bouquet bouquet = new Bouquet();
+    static Scanner sc = new Scanner(System.in);
+    static String date = sc.nextLine();
+
+
+
+    static {
+        try {
+            tulip1 = new Tulip("Tulip", 12, 40, Color.red, date);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    static {
+        try {
+            tulip2 = new Tulip("Tulip", 12, 40, Color.white, "16.05.2022");
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     public static void main(String[] args) {
-        System.out.println(tulip);
+        System.out.println(tulip1);
+        System.out.println(tulip2);
+        bouquet.AddToBouquet(tulip1);
+        bouquet.AddToBouquet(tulip2);
+        System.out.println(bouquet);
+
     }
 }
